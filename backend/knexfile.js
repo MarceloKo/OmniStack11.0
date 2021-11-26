ea90a8b398e2fec8a9e89d9ba23d7cbefde9bd1d
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-26 14:54:14 -0400

    Configurando Banco de Dados

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './src/database/db.sqlite'
     }
   },
 
@@ -13,7 +13,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -29,7 +29,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
