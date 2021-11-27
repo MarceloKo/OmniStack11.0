f3fb181505aa67196784b6ef77dd89e3b5b8af73
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-26 21:58:21 -0400

    Adicionando Ongs no Banco

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
+const { request, response } = require('express');
 const routes = express.Router();
 
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
+    return response.json(ongs);
+})
 
-routes.post('/users', (request, response) => {
-    const body = request.body;
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
+    const id = crypto.randomBytes(4).toString('HEX');
 
-    console.log(body)
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
 
-    return response.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'Marcelo Alcantara'
-    });
+    return response.json({ id });
 });
 
 module.exports = routes;
