ff00713d7f661cc876b0474055ed977b2deceb3a
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-27 08:08:13 -0400

    Rotas para fazer login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
-
 routes.get('/profile', ProfileController.index)
 
 
