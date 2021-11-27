5e0f13884e5566e72bdf653caddb9945fe891b73
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-27 14:55:23 -0400

    Conclusão página Profile

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routess() {
     return (
@@ -9,6 +10,7 @@ export default function Routess() {
             <Routes>
                 <Route path="/" exact element={<Logon />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={<Profile />} />
             </Routes>
         </Router>
     );
