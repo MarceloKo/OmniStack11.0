3df0781aba4c66ad8c84d7dd132c4fa516c76723
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-27 10:18:27 -0400

    Organizando Frontend e começando a pagina de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import './global.css';
+import Logon from "./pages/Logon";
 
 
 function App() {
+
   return (
-    <h1>Hello World</h1>
+    <h1>Hello world</h1>
   );
 }
 
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.css';
+
+
+
+export default function Logon() {
+    return (
+        <div className="logon-container">
+
+        </div>
+    );
+}
+
+
