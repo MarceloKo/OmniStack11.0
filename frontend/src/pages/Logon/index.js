05a94f934744e3ff5e3093952df7fe9325f19514
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-27 14:19:36 -0400

    Conclusão pagina registro

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FiLogIn } from 'react-icons/fi';
-
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
@@ -19,10 +19,10 @@ export default function Logon() {
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" /> Não tenho cadastro
 
-                    </a>
+                    </Link>
                 </form>
             </section>
 
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Logon from './pages/Logon';
+import Register from './pages/Register';
 
 export default function Routess() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Logon />} />
+                <Route path="/" exact element={<Logon />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
     );
