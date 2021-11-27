0c40224a2778a874967b8b7d804d17f636f17ab0
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-26 23:18:55 -0400

    Rotas para listar casos especifico de uma ONG

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.get('/profile', ProfileController.index)
+
+
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
-
 routes.delete('/incidents/:id', IncidentController.delete)
 
 module.exports = routes;
