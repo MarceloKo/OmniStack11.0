58fec12118122294e4666f2b5b3a5c021b357ab2
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-27 15:12:40 -0400

    Conclusão cadastrar novo caso

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
+
 
 export default function Routess() {
     return (
@@ -11,6 +13,8 @@ export default function Routess() {
                 <Route path="/" exact element={<Logon />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/incidents/new" element={<NewIncident />} />
+
             </Routes>
         </Router>
     );
