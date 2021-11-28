c16b70e5f793f04add7b95e7a0b0d959dcdb90e1
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-28 11:53:55 -0400

    Começando desenvolver a lista de casos da ONG

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Image, Text } from "react-native";
+import logoImg from '../../assets/logo.png';
+import styles from "./styles";
+
+export default function Incidents() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>0 casos.</Text>
+                </Text>
+
+            </View>
+            <Text style={styles.title}>Bem-vindo!</Text>
+            <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+            <View style={styles.incidentList}>
+                <View style={styles.incident}>
+                    <Text style={styles.incidentProperty}>ONG: </Text>
+                    <Text style={styles.incidentValue}>APAD </Text>
+
+                    <Text style={styles.incidentProperty}>CASO: </Text>
+                    <Text style={styles.incidentValue}>Cadelinha atropelada </Text>
+
+                    <Text style={styles.incidentProperty}>Valor: </Text>
+                    <Text style={styles.incidentValue}>R$120 </Text>
+                </View>
+            </View>
+
+        </View>
+    )
+}
