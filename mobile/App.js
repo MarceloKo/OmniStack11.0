0c99eaf208f1d6e9e02c0357986ab4ab7b45a2db
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-11-28 15:16:08 -0400

    Conectando pag detalhes ao API

--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR'
 import { StyleSheet, Text, View } from 'react-native';
 import Routes from './src/routes';
 
--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -4,13 +4,15 @@ import { Feather } from '@expo/vector-icons';
 import styles from './styles';
 import logoImg from '../../assets/logo.png';
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import * as MailComposer from 'expo-mail-composer';
 
 
 export default function Detail() {
     const navigation = useNavigation();
-    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00';
+    const route = useRoute();
+    const incident = route.params.incident;
+    const message = `Olá ${incident.name}, estou entrando em contato, pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}`;
 
     function navigateBack() {
         navigation.goBack()
@@ -18,13 +20,13 @@ export default function Detail() {
 
     function sendMail() {
         MailComposer.composeAsync({
-            subject: 'Herói do caso: cadelinha atropelada',
-            recipients: ['marceloakohlhase@gmail.com'],
+            subject: `Herói do caso: ${incident.title}`,
+            recipients: [`incident.email`],
             body: message,
         })
     }
     function sendWhatsapp() {
-        Linking.openURL(`whatsapp://send?phone=55065999189895&text=${message}`)
+        Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`)
     }
 
 
@@ -39,13 +41,15 @@ export default function Detail() {
 
             <View style={styles.incident}>
                 <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG: </Text>
-                <Text style={styles.incidentValue}>APAD </Text>
+                <Text style={styles.incidentValue}>{incident.name} de {incident.city} / {incident.uf} </Text>
 
                 <Text style={styles.incidentProperty}>CASO: </Text>
-                <Text style={styles.incidentValue}>Cadelinha atropelada </Text>
+                <Text style={styles.incidentValue}>{incident.title} </Text>
 
                 <Text style={styles.incidentProperty}>Valor: </Text>
-                <Text style={styles.incidentValue}>R$120 </Text>
+                <Text style={styles.incidentValue}>
+                    {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}
+                </Text>
 
             </View>
 
